bfc1cdf328b01ec534dc74e05441d29081d9b6b9
Rinat <r.ibragimov@pnlfin.tech>
2025-12-04 21:31:46 +0200

    UI / .onboarding(...) modifier

---
+++ Sources/UI/Onboarding.swift
@@ -0,0 +1,137 @@
+//
+//  Onboarding.swift
+//  depositario
+//
+//  Created by Rinat Ibragimov on 30.11.2025.
+//
+
+import SwiftUI
+import UI
+
+public struct OnboardingPage {
+  var prompt: String
+  var content: () -> AnyView
+
+  public init<V: View>(
+    _ prompt: String = String(localized: "app:action:next"),
+    @ViewBuilder content: @escaping () -> V
+  ) {
+    self.prompt = prompt
+    self.content = { AnyView(content()) }
+  }
+}
+
+extension View {
+  public func onboarding (
+    pages: [OnboardingPage],
+    action: @escaping () -> Void = {},
+    storageKey: String = "app:onboarding",
+  ) -> some View {
+    modifier(OnboardingController(
+      pages: pages,
+      action: action,
+      storageKey: storageKey
+    ))
+  }
+}
+
+struct OnboardingController: ViewModifier {
+  var pages: [OnboardingPage]
+  var action: () -> Void
+
+  @AppStorage var finished: Bool
+  @State var by: Bool = false
+
+  init(
+    pages: [OnboardingPage],
+    action: @escaping () -> Void = {},
+    storageKey: String
+  ) {
+    self._finished = AppStorage(wrappedValue: false, storageKey)
+    self.action = action
+    self.pages = pages
+  }
+
+  public func body(content: Content) -> some View {
+    return content
+      .onAppear {
+        if !finished {
+          by = true
+        }
+      }
+      .sheet(isPresented: $by) {
+        Onboarding(pages: pages, action: {
+          finished = true
+          action()
+        })
+      }
+      // .presentationSizing(.page.sticky(horizontal: false, vertical: true))
+      .presentationDragIndicator(.visible)
+      .presentationDetents([.large])
+  }
+}
+
+struct Onboarding: View {
+  @Environment(\.dismiss) private var dismiss
+
+  var pages: [OnboardingPage]
+  var action: () -> Void = {}
+
+  @State var selectedIndex: Int = 0
+
+  var body: some View {
+    VStack {
+      TabView(selection: $selectedIndex) {
+        ForEach(Array(pages.enumerated()), id: \.offset) { index, tab in
+          tab
+            .content()
+            .toolbar(.hidden, for: .tabBar)
+            .tag(index)
+        }
+      }
+      .tabViewStyle(.page)
+
+      if !pages.isEmpty {
+        let prompt = pages[selectedIndex].prompt
+
+        ButtonPrimary(prompt, icon: "chevron.right", reverse: true) {
+          if selectedIndex < max(0, pages.count - 1) {
+            withAnimation {
+              selectedIndex += 1
+            }
+          } else {
+            dismiss()
+            action()
+          }
+        }
+        .disabled(pages.isEmpty)
+      }
+    }
+  }
+}
+
+#Preview {
+  let pages: [OnboardingPage] = [
+    OnboardingPage("Узнать больше") {
+      OnboardingWelcome()
+    },
+    OnboardingPage {
+      OnboardingFeatures()
+    },
+    OnboardingPage("Добавить вклад") {
+      OnboardingBegin()
+    }
+  ]
+
+  Onboarding(pages: pages)
+//  let previewUserDefaults = SampleData.makeAppStorageOnboarding(onboarding: false)
+//
+//  Button("reset user defaults") {
+//    Task {
+//      previewUserDefaults.set(false, forKey: "app:onboarding")
+//    }
+//  }
+//    .onboarding()
+//    .defaultAppStorage(previewUserDefaults)
+}
+
